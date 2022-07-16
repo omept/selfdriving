57175623c1af0ac093b59a1c9d2d80ab202b4920
George Onwuasoanya <georgetheprogrammer@gmail.com>
2022-07-16 04:37:19 +0100

    added lane dash

--- js/road.js
+++ js/road.js
@@ -7,17 +7,28 @@ class Road {
         this.left = x - (width / 2)
         this.right = x + (width / 2)
 
-        const infinity = 100000000;
+        const infinity = 100000;
         this.top = -infinity;
         this.bottom = infinity;
+    }
+
+    getLaneCenter(index) {
+        const laneWidth = this.width / this.laneCount;
+        return this.left + laneWidth / 2 + laneWidth * Math.min(index, this.laneCount)
     }
 
     draw(ctx) {
         ctx.lineWidth = 5;
         ctx.strokeStyle = "white";
-        for (var i = 0; i <= this.laneCount; i++) {
-            var position = lerp(this.left, this.right, i / this.laneCount);
+
+        for (let i = 0; i <= this.laneCount; i++) {
+            const position = lerp(this.left, this.right, i / this.laneCount);
             ctx.beginPath();
+            if (i > 0 && i < this.laneCount) {
+                ctx.setLineDash([20, 20]);
+            } else {
+                ctx.setLineDash([]);
+            }
             ctx.moveTo(position, this.top);
             ctx.lineTo(position, this.bottom);
             ctx.stroke();
--- main.js
+++ main.js
@@ -3,8 +3,7 @@ canvas.width = 200;
 
 const ctx = canvas.getContext("2d")
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(100, 100, 30, 50)
-car.draw(ctx);
+const car = new Car(road.getLaneCenter(1), window.innerHeight - 50, 30, 50)
 
 animate();
 
